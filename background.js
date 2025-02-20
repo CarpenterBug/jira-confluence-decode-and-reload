915c428c11aa669de83070df0bcc5bb2588ccb09
Miguel Ângelo <3688770+CarpenterBug@users.noreply.github.com>
2025-02-20 21:53:44 +0200

    Add background.js and base functionality

---
+++ background.js
@@ -0,0 +1,13 @@
+chrome.action.onClicked.addListener((tab) => {
+    const currentUrl = new URL(tab.url);
+    const prefixToRemove = 'https://id.atlassian.com/step-up/start?continue=';
+
+    if (currentUrl.href.startsWith(prefixToRemove)) {
+        const encodedPart = currentUrl.href.replace(prefixToRemove, '');
+        const decodedUrl = decodeURIComponent(encodedPart);
+
+        chrome.tabs.update(tab.id, { url: decodedUrl });
+    } else {
+        console.log('URL does not match the expected pattern.');
+    }
+});
